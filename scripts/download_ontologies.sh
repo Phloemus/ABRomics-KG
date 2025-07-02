2847e57efb5ca6b7a919755910ffecdf4dd3f290
Phloemus <brieucquemeneur@gmail.com>
2025-07-02 16:01:36 +0200

    build: adding the ontology downloader

--- scripts/download_ontologies.sh
+++ scripts/download_ontologies.sh
@@ -4,19 +4,20 @@ FILENAME="aro.owl"
 
 FILE_PATH="$DIR/$FILENAME"
 
-if [[ -f "$FILE_PATH" ]]; then
+if [ -f "$FILE_PATH" ]; then
     echo "File '$FILENAME' already exists in '$DIR'."
     
     read -p "Do you want to delete this file? (y/n): " CONFIRM
 
-    if [[ "$CONFIRM" == "y" || "$CONFIRM" == "Y" ]]; then
+    if [ "$CONFIRM" = "y" ] || [ "$CONFIRM" = "Y" ]; then
         rm "$FILE_PATH"
+        wget https://raw.githubusercontent.com/arpcard/aro/master/aro.owl -O $FILE_PATH
         echo "New file replaced in '$FILE_PATH'"
     else
         echo "Operation cancelled. File not deleted."
     fi
 else
-    wget https://raw.githubusercontent.com/arpcard/aro/master/aro.owl
-    echo "File '$FILENAME' does not exist in '$REPO_PATH'."
+    wget https://raw.githubusercontent.com/arpcard/aro/master/aro.owl -O $FILE_PATH
+    echo "New file replaced in '$FILE_PATH'"
 fi
 
