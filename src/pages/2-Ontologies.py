b20a30ee1e52bc1732c0f001a574b3e05b409019
Phloemus <brieucquemeneur@gmail.com>
2025-08-12 15:54:02 +0200

    build: adding the small documentation of the ontologies used in abromics graph

--- src/pages/2-Ontologies.py
+++ src/pages/2-Ontologies.py
@@ -70,14 +70,30 @@ col5, col6, col7, col8 = st.columns(4)
 with col5:
     st.subheader("ARO")
     st.markdown("Antibiotic Resistance Ontology")
+    st.markdown("https://bioportal.bioontology.org/ontologies/ARO")
+    st.markdown("""
+        The Antibiotic Resistance Ontology describes antibiotic resistance genes and mutations, 
+        their products, mechanisms, and associated phenotypes, as well as antibiotics and their 
+        molecular targets. It is integrated with the Comprehensive Antibiotic Resistance Database, 
+        a curated resource containing high quality reference data on the molecular basis of 
+        antimicrobial resistance
+    """)
 
 with col6:
     st.subheader("NCIT")
     st.markdown("National Cancer Institute Thesaurus")
+    st.markdown("https://bioportal.bioontology.org/ontologies/NCIT")
+    st.markdown("""
+        Vocabulary for clinical care, translational and basic research, and public information and administrative activities
+    """)
 
 with col7:
     st.subheader("NCBITAXON")
     st.markdown("NCBI taxonomy ontology")
+    st.markdown("https://www.ncbi.nlm.nih.gov/taxonomy")
+    st.markdown("""
+        The NCBI taxonomy of species that list all the species existing
+    """)
 
 with col8:
     st.subheader("")
@@ -91,10 +107,23 @@ col9, col10, col11, col12 = st.columns(4)
 with col9:
     st.subheader("ENVO")
     st.markdown("Environmental Ontology")
+    st.markdown("https://bioportal.bioontology.org/ontologies/ENVO")
+    st.markdown("""
+        EnvO is an OBO Foundry and Library ontology for the concise, controlled description of environmental entities 
+        such as ecosystems, environmental processes, and environmental qualities. It closely interoperates with a 
+        broad collection of other OBO ontologies and is used in a diverse range of projects. 
+    """)
 
 with col10:
     st.subheader("UBERON")
     st.markdown("Uber anatomy ontology")
+    st.markdown("https://bioportal.bioontology.org/ontologies/UBERON")
+    st.markdown("""
+        Uberon is an integrated cross-species anatomy ontology representing a variety of entities classified according 
+        to traditional anatomical criteria such as structure, function and developmental lineage. The ontology 
+        includes comprehensive relationships to taxon-specific anatomical ontologies, allowing integration of 
+        functional, phenotype and expression data.
+    """)
 
 with col11:
     st.subheader("")
